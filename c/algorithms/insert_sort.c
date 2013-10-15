bf17c5ddbbbcfbe5a8b42e384cfc0bc9b335c78e
qiulin <xcodeman@gmail.com>
2013-10-15 10:23:22 +0800

    update insert sort

--- c/algorithms/insert_sort.c
+++ c/algorithms/insert_sort.c
@@ -3,7 +3,17 @@
 int arr_insert_sort();
 int link_insert_sort();
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
+
+
+int arr_insert_sort() {
+
+}
+
+
+int link_insert_sort() {
 
 }
