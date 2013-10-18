2b216dc2cab0e1f69a59d50f5bf7d61c4aabca48
qiulin <xcodeman@gmail.com>
2013-10-18 17:14:16 +0800

    update ls.c

--- c/apue/ls.c
+++ c/apue/ls.c
@@ -1,5 +1,7 @@
-#include "apue.h"
+#include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
+
 
 int
 main(int argc, char *argv[])
@@ -8,13 +10,13 @@ main(int argc, char *argv[])
     struct dirent   *dirp;
 
     if (argc != 2)
-        err_quit("Usage: ls directory_name");
+        printf("Usage: ls directory_name");
 
     if ((dp = opendir(argv[1])) == NULL)
-        err_sys("can't open %s", argv[1]);
+        printf("can't open %s", argv[1]);
     while ((dirp = readdir(dp)) != NULL)
         printf("%s\n", dirp->d_name);
 
     closedir(dp);
-    exit(0);
+    return 0;
 }
