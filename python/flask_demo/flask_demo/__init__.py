e21907d1d008df5c8ce8d884bdfb61ec19235b51
qiulin <xcodeman@gmail.com>
2013-11-22 17:36:48 +0800

    udpate

--- python/flask_demo/flask_demo/__init__.py
+++ python/flask_demo/flask_demo/__init__.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+doc
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = 'fjaeojfaojeoan3091u4jdsaou302r4jefaou0u0wqraofjef'
+
+
+from flask_demo import views
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
