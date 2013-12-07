8060e32bd4be6525c09a9db6a1f1a04be11707fb
qiulin <xcodeman@gmail.com>
2013-12-07 14:45:03 +0800

    django start new app blog

--- python/django_demo/auth/views.py
+++ python/django_demo/auth/views.py
@@ -1,21 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django import forms
 
 def index(request):
-    welcome_msg = {"w": _(u"欢迎您，游客")}
-    if request.user.is_authenticated():
-        welcome_msg['w'] = _(u"欢迎您 %s!") % request.user.username
-    return render(request, 'auth/index.html', welcome_msg)
-
+    '''用户的个人页面
+    '''
+    return render(request, 'auth/index.html')
 
 def register(request):
-    pass
-
+    '''
+    注册'''
 
 def login(request):
-    pass
+     if requset.method == 'GET':
+        form = LoginForm()
+        return render(request, 'login')
+
+   pass
 
 
 def logout(request):
     pass
+
+
+# 登录表单
+class LoginForm(forms.Form):
+    '''Login From'''
+    error = forms.CharField()
+    username = forms.CharField(label=u"昵称", max_length=30, widget=forms.TextInput(attrs={'size': 20,}))
+    password = forms.PasswordInput(label=u'密码', max_length=30, widget=forms.PasswordInput(attrs={'size': 20,}))
