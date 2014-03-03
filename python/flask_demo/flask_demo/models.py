5bee5b9aeda507d436857a0b4e2bb5dde9f094a1
qiulin <xcodeman@gmail.com>
2014-03-03 21:17:37 +0800

    define post model and update User model __init__ funciton

--- python/flask_demo/flask_demo/models.py
+++ python/flask_demo/flask_demo/models.py
@@ -1,26 +1,38 @@
 from flask_demo import db
-from hashlib import md5
+from flask_demo.utils import password_encrypt
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(128), unique=True)
+    password = db.Column(db.String(128))
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        self.password = self.password_encrypt(password)
+        self.password = password_encrypt(password)
 
     def __repr__(self):
         return '<User %r>' % self.username
-
-    def password_encrypt(self, password):
-        en_password = md5(password)
-        return en_password.hexdigest()
 
     def is_active(self):
         return True
 
     def get_id(self):
         return unicode(id)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey("User.id"))
+    publish_datetime = db.Column(db.DateTime)
+    title = db.Column(db.Text)
+    markdown = db.Column(db.Text)
+    html = db.Column(db.Text)
+
+    def __init__(self, uid, publish_datetime, title, markdown):
+        self.uid = uid
+        self.publish_datetime = publish_datetime
+        self.title = title
+        self.markdown = markdown
